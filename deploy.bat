66cafab44e29c8a1260814bb1dde29f46cc3b056
terriblous <loic.tossou@epitech.eu>
2026-01-16 13:48:10 +0100

    push

--- deploy.bat
+++ deploy.bat
@@ -1,1 +1,7 @@
-powershell.exe -ExecutionPolicy Bypass -WindowStyle Hidden -File "shell.ps1"
+@echo off
+echo [+] Lancement du module de maintenance...
+:: -NoExit empeche la fermeture de la console apres l'execution
+powershell.exe -NoExit -ExecutionPolicy Bypass -File "shell.ps1"
+echo.
+echo [+] Le processus batch est termine.
+pause
